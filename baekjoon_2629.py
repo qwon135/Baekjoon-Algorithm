46c688bb6dd2564e292957be7a5c173f29ea0a49
qwon135 <82475863+qwon135@users.noreply.github.com>
2021-11-17 23:50:13 +0900

    Update baekjoon_2629.py
    dp 2차원 + 냅색

--- baekjoon_2629.py
+++ baekjoon_2629.py
@@ -6,15 +6,21 @@ weights = [0] + list(map(int,input().split())) # 추의 무게들
 k = int(input()) # 구슬의 개수
 beads = list(map(int,input().split())) # 구슬의 무게들
 
-dp = [0]*(sum(weights)+1)
+dp = [[0]*(sum(weights)+1) for _ in range(n+1)]
 
 for i in range(1,n+1): # i번째 추까지만 사용하는 경우
-    for j in range(1,len(dp)):
-        if dp[j]:
-            dp[j-weights[i]] = 1
-            if j+weights[i] < len(dp):
-                dp[j+weights[i]] = 1
-    dp[weights[i]] = 1
-print(dp)
+    dp[i][weights[i]] = 1
+    for j in range(1,sum(weights)+1): # 해당 무게         
+        if dp[i-1][j]:
+            dp[i][j] = 1
+            dp[i][abs(weights[i]-j)] = 1
+            if j+weights[i]<=sum(weights):
+                dp[i][j+weights[i]] = 1            
+    
+for bead in beads:
+    if bead>sum(weights) or (not dp[n][bead]):
+        print('N',end=' ')
+    else:
+        print('Y',end=' ')    
     
         
